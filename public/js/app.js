a6ad9ae2e388ed151a3e23f53ad9b46cefaa31f1
Giorgos Petrou <gdpetrou@csd.auth.gr>
2019-04-16 15:02:20 +0300

    app.js config

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('client side file');
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message1')
