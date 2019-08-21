db045857f485beb83bf2dcb42ecd2b25f5548d59
Giorgos Petrou <gdpetrou@csd.auth.gr>
2019-08-21 12:54:12 +0300

    config of env variables

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,16 @@
 const request = require('request')
+require('dotenv').config()
 
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/eb3f7530d9d1ba420fb653afaec8e6d0/' + latitude +',' + longitude +'?units=si'
+    const url = 'https://api.darksky.net/forecast/' + process.env.FORECAST_API_KEY + '/' + latitude +',' + longitude +'?units=si'
     request( { url, json: true}, (error, {body}) => {
       if (error) {
         callback('Unable to connect to Darksky' , undefined)
       } else if (body.error) {
         callback(body.error, undefined)
       } else {
-        callback(undefined, body.daily.data[0].summary +" It's currently " + body.currently.temperature + " °C out with a high of " + body.daily.data[0].temperatureHigh + " °C and a low of " + body.daily.data[0].temperatureLow + " °C. There is a " + body.currently.precipProbability + "% chance of rain and the humidity will be at " + body.daily.data[0].humidity + "% for today. " )
+        callback(undefined, body.daily.data[0].summary +" It's currently " + body.currently.temperature + " °C out with a high of " + body.daily.data[0].temperatureHigh + " °C and a low of " + body.daily.data[0].temperatureLow + " °C. There is a " + body.currently.precipProbability + "% chance of rain and the humidity will be at " + body.daily.data[0].humidity*100 + "% for today. " )
 
       }
     })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,22 +1,24 @@
 const request = require('request')
-
+require('dotenv').config()
 
 
 const geocode = (address, callback)=> {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoidHppcGV0IiwiYSI6ImNqdWI3czA3YjA3YXA0NGx0NjNmMGxpcXoifQ.llj7dVqziUJ4-_ECUTGDtw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=' + process.env.GEOCODE_API_KEY + '&limit=1'
 
     request ({ url, json: true}, (error, {body}) => {
       if (error) {
-        callback('Unable to connect to location services', undefined)
-      } else if (body.features.length == 0) {
-        callback('Unable to find location. Try another search', undefined)
-      } else {
-        callback(undefined, {
+        return callback('Unable to connect to location services', undefined)
+      }
+      if (body && body.features && body.features.length === 0) {
+        return callback('Unable to find location. Try another search', undefined)
+      }
+
+      return callback(undefined, {
           latitude: body.features[0].center[1],
           longitude: body.features[0].center[0],
           location: body.features[0].place_name
         })
-      }
+
     })
 
 }
